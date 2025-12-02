99eb68dbd874b909a85ca50be6e5cb27394429e7
dev2 <juli-felix@outlook.com>
2025-12-02 18:04:20 -0300

    Live coding section part 1

---
+++ lecture2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+class Student
+{   
+    int id = 0;
+    std::string m_first = "First name";
+    std::string m_last  = "Last name";
+    int         m_id    = 0;
+    float       m_avg   = 0;
+
+public:
+
+    Student(){}
+
+    Student(std::string first, std::string last, int id, float avg)
+    : m_first (first)
+    , m_last  (last)
+    , m_id    (id)
+    , m_avg   (avg)
+    {
+    }
+
+    int getAvg() const
+    {
+        return m_avg;
+    }
+
+    int getID() const
+    {
+        return m_id;
+    }
+
+    std::string getFirst() const
+    {
+        return m_first;
+    }
+
+    std::string getLast() const
+    {
+        return m_last;
+    }
+
+    void print() const
+    {
+        std::cout << m_first << " " << m_last << " " << m_id << " " << m_avg << "\n";
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Student s1;
+    Student s2("Juli", "Félix", 1, 98.9);
+    Student s3("Jane", "Doe", 2, 100.0);
+
+    s3.print();
+
+    return 0;
+}
