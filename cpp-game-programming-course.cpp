44058606deb1c83ec0a4be3024f626f67afcc76d
unknown <juli-felix@outlook.com>
2025-12-05 14:03:17 -0300

    Finished the introductory lesson about c++ talking about pointers and templates

--- cpp-game-programming-course.cpp
+++ cpp-game-programming-course.cpp
@@ -95,11 +95,71 @@ public:
     }
 };
 
+template <typename T>
+
+class DynamicArray
+{
+    T*   m_arr;
+    size_t m_size;
+
+public:
+
+    DynamicArray(size_t size)
+        : m_arr(new T[size])
+        , m_size(size)
+    {
+        std::cout << "Array Constructor\n";
+    }
+
+    ~DynamicArray()
+    {
+        delete[] m_arr;
+        std::cout << "Array Destructor\n";
+    }
+
+    T get(size_t index) const
+    {
+        return m_arr[index];
+    }
+
+    void set(size_t index, T val)
+    {
+        m_arr[index] = val;
+    }
+
+    void print() const
+    {
+        for (size_t i = 0; i < m_size; i++)
+        {
+            std::cout << i << " " << m_arr[i] << "\n";
+        }
+    }
+
+    const T& operator [](size_t index) const
+    {
+        return m_arr[index];
+    }
+
+    T& operator [](size_t index)
+    {
+        return m_arr[index];
+    }
+};
+
+void pp(int & i)
+{
+    std::cout << &i << " " << sizeof(i) << " " << i << "\n";
+}
+
 int main(int argc, char* argv[])
 {
-    Course c("COMP 4300");
-    c.loadFromFile("students.txt");
-    c.print();
+    DynamicArray<float> myArray(10);
+
+    myArray[2] = 2;
+
+    std::cout << myArray[3] << "\n";
+
+    myArray.print();
 
     return 0;
 }
